35fcb2944de634faa28dc103eb2f7af145a684ac
zhenqi <dreamingwzq@gmail.com>
2023-12-09 17:26:42 +0800

    [feat][REFACTOR][WIP] replace if-statements with dict.

--- cmd_arg_parser/src/my_app.py
+++ cmd_arg_parser/src/my_app.py
@@ -13,14 +13,22 @@ class Options(BaseModel):
 
 def parse_arg(arguments: List[str]):
     options = Options()
-    for index, arg in enumerate(iter(arguments)):
-        if "-l" == arg:
-            options.logging = True
-        if "-p" == arg:
-            options.port = int(arguments[index + 1])
-        if "-d" == arg:
-            options.directory = arguments[index + 1]
+    process_arg_list(arguments, options)
     return options
+
+
+def process_arg_list(arguments, options):
+    arg_mapping = {
+        "-l": ("logging", lambda i: True),
+        "-p": ("port", lambda i: int(arguments[i + 1])),
+        "-d": ("directory", lambda i: arguments[i + 1]),
+    }
+
+    for index, arg in enumerate(iter(arguments)):
+        if arg in arg_mapping:
+            attr, method = arg_mapping[arg]
+            setattr(options, attr, method(index))
+
 
 if __name__ == "__main__":
     print(parse_arg(sys.argv[1:]))
