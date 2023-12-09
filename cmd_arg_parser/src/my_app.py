79fdd82fd9c87efd303556ca1cffefe6d8667599
zhenqi <dreamingwzq@gmail.com>
2023-12-09 16:37:21 +0800

    [feat][GREEN] developing functionality point "happy_path.multiple_single_valued_arguments".

--- cmd_arg_parser/src/my_app.py
+++ cmd_arg_parser/src/my_app.py
@@ -1,9 +1,11 @@
 def parse_arg(args: str):
     return_values = [False, 0, "", [], []]
-    if "-l" in args:
-        return_values[0] = True
-    if "-p" in args:
-        return_values[1] = int(args[args.index("-p") + 3:])
-    if "-d" in args:
-        return_values[2] = args[args.index("-d") + 3:]
+    arg_list = args.split("-")
+    for arg in arg_list:
+        if arg.startswith("l"):
+            return_values[0] = True
+        if arg.startswith("p"):
+            return_values[1] = int(arg[2:])
+        if arg.startswith("d"):
+            return_values[2] = arg[2:]
     return return_values
