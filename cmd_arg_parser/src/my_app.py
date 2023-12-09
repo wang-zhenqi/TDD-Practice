6eab37f71519b1527e779ac128ca35ff1d24d2a0
zhenqi <dreamingwzq@gmail.com>
2023-12-09 16:13:43 +0800

    [feat][GREEN] developing functionality point "happy_path.single_argument_with_single_integer_values".

--- cmd_arg_parser/src/my_app.py
+++ cmd_arg_parser/src/my_app.py
@@ -2,4 +2,6 @@ def parse_arg(args: str):
     return_values = [False, 0, "", [], []]
     if "-l" in args:
         return_values[0] = True
+    if "-p" in args:
+        return_values[1] = int(args[args.index("-p") + 2:])
     return return_values
