5091585c51c98d431c31d11a057933966fa3698f
zhenqi <dreamingwzq@gmail.com>
2023-12-09 16:07:45 +0800

    [feat][GREEN] developing functionality point "happy_path.single_argument_with_single_boolean_values".

--- cmd_arg_parser/src/my_app.py
+++ cmd_arg_parser/src/my_app.py
@@ -1,2 +1,5 @@
 def parse_arg(args: str):
-    return [False, 0, "", [], []]
+    default_values = [False, 0, "", [], []]
+    if "-l" in args:
+        default_values[0] = True
+    return default_values
