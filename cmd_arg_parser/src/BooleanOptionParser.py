1781203c3de67de0434d2ce1278667bf0bb34ddd
zhenqi <dreamingwzq@gmail.com>
2024-03-02 18:20:49 +0800

    [feat][GREEN] finish the functionality

--- cmd_arg_parser/src/BooleanOptionParser.py
+++ cmd_arg_parser/src/BooleanOptionParser.py
@@ -3,4 +3,6 @@ from OptionParser import OptionParser
 
 class BooleanOptionParser(OptionParser):
     def parse(self, index, argument_list):
+        if len(argument_list) - index > 1 and not argument_list[index + 1].startswith("-"):
+            raise TypeError(f"Too many arguments for the boolean option {argument_list[index]}")
         return True
