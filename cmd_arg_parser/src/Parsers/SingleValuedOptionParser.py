c3aca5a690e4ed1b99018f11011d99658c0f0130
zhenqi <dreamingwzq@gmail.com>
2024-03-10 16:29:48 +0800

    [feat][REFACTOR] extract argument quantity checking method

--- cmd_arg_parser/src/Parsers/SingleValuedOptionParser.py
+++ cmd_arg_parser/src/Parsers/SingleValuedOptionParser.py
@@ -3,12 +3,20 @@ from Exceptions.TooManyArgumentsException import TooManyArgumentsException
 from OptionParser import OptionParser
 
 
+def too_many_arguments(argument_list, index):
+    return len(argument_list) - index > 2 and not argument_list[index + 2].startswith("-")
+
+
+def not_sufficient_arguments(argument_list, index):
+    return (len(argument_list) - index > 1 and argument_list[index + 1].startswith("-")) or \
+        len(argument_list) - index == 1
+
+
 class SingleValuedOptionParser(OptionParser):
     def parse(self, index, argument_list):
-        if len(argument_list) - index > 2 and not argument_list[index + 2].startswith("-"):
+        if too_many_arguments(argument_list, index):
             raise TooManyArgumentsException(argument_list[index])
-        if (len(argument_list) - index > 1 and argument_list[index + 1].startswith("-")) or \
-                len(argument_list) - index == 1:
+        if not_sufficient_arguments(argument_list, index):
             raise NotSufficientArgumentException(argument_list[index])
         try:
             result = self.parsing_function(argument_list[index + 1])
