12d73478b129e7aa5d51fe7c4aa5ecf30218b67e
zhenqi <dreamingwzq@gmail.com>
2024-09-17 03:03:13 +0800

    [feat] add the whole tests for sending emails [doc] change the description of the tasks

--- self_practices/birthday_greetings/test/test_birthday_greetings.py
+++ self_practices/birthday_greetings/test/test_birthday_greetings.py
@@ -31,35 +31,51 @@ Happy Birthday, dear <first_name>!
 
 ## Tasks
 
-- [ ] Write a program that reads the database and sends birthday greetings to all employees whose birthday is today.
-    - [ ] Reading the database
-        - [ ] The program should connect to the database
-        - [ ] The program should retrieve the employees whose birthday is today
-        - [ ] The program should return nothing when there are no employees whose birthday is today
-    - [ ] Sending birthday greetings
-        - [ ] The program should send a birthday greeting email to all employees whose birthday is today when such employees exist
-            - [ ] The email should contain the message described above
-            - [ ] The email should be sent to the employee's email address
-        - [ ] The program should not send any email when there are no employees whose birthday is today
-            - [ ] The program should print "No employees whose birthday is today"
+- [x] Write a program that reads the database and sends birthday greetings to all employees whose birthday is today.
+    - [x] Connecting to the mysql database
+        - [x] The program should connect to the mysql database on localhost:3306
+        - [x] The program should retrieve the employees whose birthday is today
+        - [x] The program should return nothing when there are no employees whose birthday is today
+    - [x] Generating birthday greetings
+        - [x] The program should generate emails to all employees whose birthday is today when such employees exist
+            - [x] The email body should contain the message described above
+            - [x] The recipient of the email should be the employee's email address
 """
+from datetime import datetime
+
 import pytest
 
+from birthday_greetings import generate_message
+from database_repository import DatabaseRepository
 
-@pytest.mark.skip
+
+@pytest.fixture(scope="module")
+def db_cnx():
+    return DatabaseRepository(
+        "localhost",
+        3306,
+        "root",
+        "B9Lz_XFEKh",
+        "BirthdayGreetings",
+        "Employees"
+    )
+
+
+@pytest.fixture(scope="module")
+def today():
+    return datetime.today().strftime("%Y-%m-%d")
+
+
 class TestBirthdayGreetings:
-    def test_should_send_email_when_there_are_employees_whose_birthday_is_today(self):
-        pass
+    def test_should_send_email_when_there_are_employees_whose_birthday_is_today(self, db_cnx, today):
+        employees = db_cnx.get_employees_whose_birthday_is(today)
+        email_contents = [generate_message(employee) for employee in employees]
+        assert len(email_contents) == 1
+        assert len(employees) == 1
+        assert employees[0].first_name == "Peter"
+        assert email_contents[0].recipient == "peter.smith@gmail.com"
+        assert email_contents[0].body == "Happy Birthday, dear Peter!"
 
-    def test_should_not_send_email_when_there_are_no_employees_whose_birthday_is_today(self):
-        pass
-
-    def test_should_send_email_with_message_containing_employee_first_name(self):
-        pass
-
-    def test_should_send_email_to_employee_email_address(self):
-        pass
-
-    def test_should_print_no_employees_whose_birthday_is_today_when_there_are_no_employees_whose_birthday_is_today(
-            self):
-        pass
+    def test_should_not_send_email_when_there_are_no_employees_whose_birthday_is_today(self, db_cnx):
+        employees = db_cnx.get_employees_whose_birthday_is("2024-09-11")
+        assert len(employees) == 0
