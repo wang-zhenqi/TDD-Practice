b911884454b684d8a71733cc0cc1f0ddc9310427
zhenqi <dreamingwzq@gmail.com>
2024-09-18 08:52:39 +0800

    [chore] add a testing file, not developed yet

---
+++ self_practices/birthday_greetings/test/test_object_relational_mapping.py
@@ -0,0 +1,33 @@
+"""
+# Object Relational Mapping
+
+## Objectives
+
+- Being able to connect to the database using SQLAlchemy
+- Being able to read data from files using Pandas
+- Retrieve the data of employees whose birthday is today to Employee objects
+
+## Tasks
+
+- [ ] Connect to the database using SQLAlchemy
+    - [ ] The program should connect to the mysql database on localhost:3306
+    - [ ] Retrieve the data of employees whose birthday is today
+- [ ] Read data from a file using Pandas
+    - [ ] The program should read data from a csv file
+- [ ] Retrieve the data of employees whose birthday is today to Employee objects
+    - [ ] The program should return the data of employees whose birthday is today to Employee objects
+"""
+
+
+class TestObjectRelationalMapping:
+    def test_should_connect_to_the_database(self):
+        pass
+
+    def test_should_retrieve_the_data_of_employees_whose_birthday_is_today(self):
+        pass
+
+    def test_should_read_data_from_a_file(self):
+        pass
+
+    def test_should_return_the_data_of_employees_whose_birthday_is_today_to_employee_objects(self):
+        pass
